7b35d3a9394b6241ee7da80972ca5bc2220b9286
pkm1614 <pkm1614@gmail.com>
2022-09-19 16:53:16 +0900

    2022-09-19 : using controller

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pkm16
+ * Date: 2022-09-19
+ * Time: 오후 3:59
+ * Comments:
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public  String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
